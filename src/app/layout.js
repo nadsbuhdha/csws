c03d9ba49cbdbb7b7384818719970db215948600
Nads Buhdha <nadsbuhdha@nadss-mbp.home>
2024-03-22 20:09:01 +0000

    remove script

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
       <body className={`flex flex-col min-h-screen ${opensans.variable}`}> <Navbar />{children}
       <Footer />
       
-      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+      {/* <script src="../path/to/flowbite/dist/flowbite.min.js"></script> */}
       </body>
     </html>
   );
